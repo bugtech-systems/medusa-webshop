19fb808d55482f7222279af03c7492c0283abd8b
jaybee <jb.geli@psa.org.au>
2026-01-10 12:20:54 +0800

    Fix login

--- medusa-config.ts
+++ medusa-config.ts
@@ -8,12 +8,16 @@ export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS || "https://*.ngrok-free.app",
-      adminCors: process.env.ADMIN_CORS || "https://*.ngrok-free.app",
-      authCors: process.env.AUTH_CORS || "https://*.ngrok-free.app",
+      storeCors: process.env.STORE_CORS || "*",
+      adminCors: process.env.ADMIN_CORS || "*",
+      authCors: process.env.AUTH_CORS || "*",
       jwtSecret: process.env.JWT_SECRET || "psawebshop",
       cookieSecret: process.env.COOKIE_SECRET || "psawebshop",
     },
+    cookieOptions: {
+      secure: true, // Set to false for HTTP on localhost
+      sameSite: "none", // or "none" if required; see Step 4
+    }
   },
 
   modules: {
