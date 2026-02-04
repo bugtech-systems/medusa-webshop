2a50d6abef15e74a472a68d5bcc5ea47dd2e7b47
jaybee <jb.geli@psa.org.au>
2026-02-05 02:15:02 +0800

    fix medusa config path

--- apps/backend/src/links/cart-approval-status.ts
+++ apps/backend/src/links/cart-approval-status.ts
@@ -1,5 +1,5 @@
 import { defineLink } from "@medusajs/framework/utils";
-import ApprovalModule from "@/modules/approval";
+import ApprovalModule from "../modules/approval";
 import CartModule from "@medusajs/medusa/cart";
 
 export default defineLink(CartModule.linkable.cart, {
