93114647b57d2b7420315538c0f6206619a4bd63
jaybee <jb.geli@psa.org.au>
2026-02-05 01:47:41 +0800

    change db to supabase

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -9,7 +9,7 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
 export default defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl: process.env.SUPABASE_DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS || "*",
       adminCors: process.env.ADMIN_CORS || "*",
--- apps/backend/src/modules/quote/migrations/Migration20260204165856.ts
+++ apps/backend/src/modules/quote/migrations/Migration20260204165856.ts
@@ -1,6 +1,6 @@
 import { Migration } from "@medusajs/framework/mikro-orm/migrations";
 
-export class Migration20260126001928 extends Migration {
+export class Migration20260204165856 extends Migration {
 
   override async up(): Promise<void> {
     this.addSql(`create table if not exists "quote" ("id" text not null, "status" text check ("status" in ('pending_merchant', 'pending_customer', 'accepted', 'customer_rejected', 'merchant_rejected')) not null default 'pending_merchant', "customer_id" text not null, "draft_order_id" text not null, "order_change_id" text not null, "cart_id" text not null, "created_at" timestamptz not null default now(), "updated_at" timestamptz not null default now(), "deleted_at" timestamptz null, constraint "quote_pkey" primary key ("id"));`);
