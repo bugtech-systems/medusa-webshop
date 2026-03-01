f0018d09192acf1c52d1576c49353d61391f655f
jaybee <jb.geli@psa.org.au>
2026-03-02 07:00:21 +0800

    Fix dashboards widgets

--- apps/backend/src/links/employee-customer.ts
+++ apps/backend/src/links/employee-customer.ts
@@ -1,6 +1,6 @@
 import { defineLink } from "@medusajs/framework/utils";
 import CustomerModule from "@medusajs/medusa/customer";
-import CompanyModule from "@/modules/company";
+import CompanyModule from "../modules/company";
 
 export default defineLink(
   CompanyModule.linkable.employee,
