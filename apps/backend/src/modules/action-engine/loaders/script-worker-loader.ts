10bd2685622bb13591a23df0dfd89a7da104b452
jaybee <jb.geli@psa.org.au>
2026-03-12 07:45:33 +0800

    Fix event listeners

--- apps/backend/src/modules/action-engine/loaders/script-worker-loader.ts
+++ apps/backend/src/modules/action-engine/loaders/script-worker-loader.ts
@@ -33,7 +33,10 @@ export default async function piscinaWorkerLoader({
      */
     const workerPath = workerOptions.workerPath
       ? path.resolve(workerOptions.workerPath)
-      : require.resolve("../services/script-worker");
+      : path.resolve(
+        __dirname,
+        "../services/script-worker.js"
+      );
 
     logger?.info(`[Piscina] Resolved worker path: ${workerPath}`);
 
