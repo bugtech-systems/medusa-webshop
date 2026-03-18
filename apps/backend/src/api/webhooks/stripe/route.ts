1ff502e77a7acab0d600423804df6d4685418e91
jaybee <jb.geli@psa.org.au>
2026-03-18 08:04:25 +0800

    fix payment link

--- apps/backend/src/api/webhooks/stripe/route.ts
+++ apps/backend/src/api/webhooks/stripe/route.ts
@@ -2,7 +2,7 @@ import { MedusaRequest, MedusaResponse } from "@medusajs/framework";
 import Stripe from "stripe";
 import { Modules } from "@medusajs/framework/utils";
 
-const stripe = new Stripe(process.env.STRIPE_API_KEY!, { apiVersion: "2025-09-30.clover" });
+const stripe = new Stripe(process.env.STRIPE_API_KEY!, { apiVersion: "2023-10-16" as any });
 
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
   const sig = req.headers["stripe-signature"] as string;
@@ -10,6 +10,10 @@ export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
   let event: Stripe.Event;
 
   try {
+
+    console.log(req, "STRIPE REQ")
+
+
     event = stripe.webhooks.constructEvent(
       req.rawBody,
       sig,
